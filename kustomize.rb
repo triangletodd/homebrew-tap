de6a88704a9dcb2f7d45664a8de33322a99b2edf
Todd Edwards <todd.edwards@vitalsource.com>
2018-08-07 13:28:40 -0400

    Add -ldflags.

--- kustomize.rb
+++ kustomize.rb
@@ -12,10 +12,12 @@ class Kustomize < Formula
     ENV["GOOS"] = 'darwin'
     ENV["GOARCH"] = 'amd64'
     ENV["CGO_ENABLED"] = '1'
+
+    GO_BUILD_LDFLAGS="-s -w -X github.com/kubernetes-sigs/kustomize/pkg/commands.kustomizeVersion=1.0.5"
+
     (buildpath/"src/github.com/kubernetes-sigs/kustomize/").install buildpath.children
     cd "src/github.com/kubernetes-sigs/kustomize/" do
-      system "go", "build", "-v", "-o", "kustomize",
-        "-X", "github.com/kubernetes-sigs/kustomize/pkg/commands.kustomizeVersion=1.0.5"
+      system "go build -v -o kustomize -ldflags ${GO_BUILD_LDFLAGS}"
       bin.install "kustomize"
       pkgshare.install Dir["examples/*"]
       prefix.install_metafiles
