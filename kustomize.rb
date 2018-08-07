4117c4cd030d1730db90f52f4779b520b020a3c8
Todd Edwards <todd.edwards@vitalsource.com>
2018-08-07 10:03:58 -0400

    Kustomize: add sha256 sum

--- kustomize.rb
+++ kustomize.rb
@@ -2,6 +2,7 @@ class Kustomize < Formula
   desc "Customization of kubernetes YAML configurations"
   homepage "https://github.com/kubernetes-sigs/kustomize"
   url "https://github.com/kubernetes-sigs/kustomize/archive/v1.0.5.tar.gz"
+  sha256 "2afbd060c831aa850d739a83bb5376d2c8c5591ccbbac7be0ae785022aaed5f7"
   version "1.0.5"
 
   depends_on "go" => :build
