8ca4a2451b587087fcde273a853e71df4ef00a38
Todd Edwards <todd.edwards@vitalsource.com>
2018-08-07 15:41:07 -0400

    Revert "Brew audit cleanup."
    This reverts commit 9113f938cf40563ebabfa62267b3d04ee558dfe3.

--- kustomize.rb
+++ kustomize.rb
@@ -5,30 +5,31 @@ class Kustomize < Formula
   homepage "https://github.com/kubernetes-sigs/kustomize"
   url "https://github.com/kubernetes-sigs/kustomize/archive/v1.0.5.tar.gz"
   sha256 "2afbd060c831aa850d739a83bb5376d2c8c5591ccbbac7be0ae785022aaed5f7"
+  version "1.0.5"
 
   depends_on "go" => :build
 
   def install
-    build_date = Time.now.utc.iso8601
+    buildDate = Time.now.utc.iso8601
 
     ENV["GOPATH"] = buildpath
-    ENV["GOOS"] = "darwin"
-    ENV["GOARCH"] = "amd64"
-    ENV["CGO_ENABLED"] = "1"
+    ENV["GOOS"] = 'darwin'
+    ENV["GOARCH"] = 'amd64'
+    ENV["CGO_ENABLED"] = '1'
 
     go_build_ldflags = [
       "-X github.com/kubernetes-sigs/kustomize/pkg/commands.kustomizeVersion=#{version}",
       "-X github.com/kubernetes-sigs/kustomize/pkg/commands.goos=#{ENV['GOOS']}",
       "-X github.com/kubernetes-sigs/kustomize/pkg/commands.goarch=#{ENV['GOARCH']}",
       "-X github.com/kubernetes-sigs/kustomize/pkg/commands.gitCommit=#{version}",
-      "-X github.com/kubernetes-sigs/kustomize/pkg/commands.buildDate=#{build_date}",
-    ].join(" ")
+      "-X github.com/kubernetes-sigs/kustomize/pkg/commands.buildDate=#{buildDate}"
+    ].join(' ')
 
     (buildpath/"src/github.com/kubernetes-sigs/kustomize/").install buildpath.children
     cd "src/github.com/kubernetes-sigs/kustomize/" do
       system "go build -v -o kustomize -ldflags \"#{go_build_ldflags}\""
       bin.install "kustomize"
-      pkgshare.install "examples"
+      pkgshare.install Dir["examples"]
       prefix.install_metafiles
     end
   end
