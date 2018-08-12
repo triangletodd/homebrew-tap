f20398f8a7f0c849d69e57f32a04ae6cd4d4c141
Todd Edwards <todd.edwards@vitalsource.com>
2018-08-12 16:58:19 -0400

    Add sha, commit and date to build

--- lazygit.rb
+++ lazygit.rb
@@ -1,7 +1,9 @@
 require 'time'
+require 'json'
+require 'net/http'
 
 class Lazygit < Formula
-  desc "Customization of kubernetes YAML configurations"
+  desc "A simple terminal UI for git commands, written in Go with the gocui library"
   homepage "https://github.com/jesseduffield/lazygit/"
   url "https://github.com/jesseduffield/lazygit/archive/v0.1.55.tar.gz"
   sha256 "c77f6f2176a850d324992e502e6088a3d2dbc4faa15898af24d68b01e33acef3"
@@ -12,22 +14,25 @@ class Lazygit < Formula
   def install
     build_date = Time.now.utc.iso8601
 
+    sha_uri = URI("https://api.github.com/repos/jesseduffield/lazygit/git/refs/tags/v#{version}")
+    sha_json = Net::HTTP.get(sha_uri)
+    sha_obj = JSON.parse(sha_json)
+    sha = sha_obj['object']['sha']
+
+
     ENV["GOPATH"] = buildpath
     ENV["GOOS"] = 'darwin'
     ENV["GOARCH"] = 'amd64'
     ENV["CGO_ENABLED"] = '1'
 
-    cd "src/github.com/jesseduffield/lazygit/" do
-      commit_hash=`git rev-parse HEAD`
-    end
+    (buildpath/"src/github.com/jesseduffield/lazygit/").install buildpath.children
 
     go_build_ldflags = [
-      "-X github.com/jesseduffield/lazygit/main.buildDate=#{build_date}",
-      "-X github.com/jesseduffield/lazygit/main.version=0.1.55",
-      "-X github.com/jesseduffield/lazygit/main.commit=#{commit_hash}",
+      "-X main.date=#{build_date}",
+      "-X main.version=#{version}",
+      "-X main.commit=#{sha}",
     ].join(' ')
 
-    (buildpath/"src/github.com/jesseduffield/lazygit/").install buildpath.children
     cd "src/github.com/jesseduffield/lazygit/" do
       system "go build -v -o lazygit -ldflags \"#{go_build_ldflags}\""
       bin.install "lazygit"
